978f96b01d3cb063f127464cfff8d2c2d1ed34e2
Siu <598166242@qq.com>
2018-08-31 15:28:55 +0800

    v1.0.1

--- RoomMaster.py
+++ RoomMaster.py
@@ -7,12 +7,11 @@ import json
 import io
 
 CONTROLLER_GROUP_NAME = "房管控制台"
-# TARGET_GROUP_NAME = "涂鸦战士绝武互刷群"
-TARGET_GROUP_NAME = "房管控制台"
+TARGET_GROUP_NAME = "涂鸦战士绝武互刷群"
+# TARGET_GROUP_NAME = "房管控制台"
 MAX_MEMBER_NUM = 4
 
 DATA_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), "Plugins", "data.json").replace('\\', '/')
-# DATA_PATH = 'data.json'
 
 NEW_MSG = '''新房管的功能可以说强大了不少，基本上连松鼠都能用，不会再有太多的指令无法识别的问题了，除非你是真的在乱搞（bug是没有的，这辈子都不会有的）
 
@@ -174,8 +173,8 @@ def checkGroupFull(weaponName):
 
 
 def onPlug(bot):
-    sendMsgToController(bot, "房管上线成功")
-    sendMsgToController(bot, DATA_PATH)
+    sendMsgToController(bot, "房管已上线")
+    # sendMsgToController(bot, DATA_PATH)
     getFormerRecord(bot)
     sendMsgToController(bot, getGroupInfo())
 
